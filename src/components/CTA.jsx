107d76392bace2a5319c582f4a76a4920c0b7998
Michelle <data_Michelle@proton.me>
2025-05-27 09:04:50 -0500

    final layout for CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,26 +3,29 @@ import Button from "./Button";
 
 const CTA = () => {
   return (
-    <section className="cta-section" aria-labelledby="cta-heading">
-      <div className="container">
-        <article className="cta-content">
-          <header>
-            <h1 id="cta-heading">Little Lemon</h1>
-            <h2>Chicago</h2>
-          </header>
-          <section>
-            <p>
-              We are a family owned Mediterranean restaurant, focused on
-              traditional recipes served with a modern twist.
-            </p>
-            <Button label="Reserve a Table" />
-          </section>
-        </article>
+    <>
+      <section className="cta-section" aria-labelledby="cta-heading">
+        <div className="container">
+          <article className="cta-content">
+            <header>
+              <h1 id="cta-heading">Little Lemon</h1>
+              <h2>Chicago</h2>
+            </header>
+            <section>
+              <p>
+                We are a family owned Mediterranean restaurant, focused on
+                traditional recipes served with a modern twist.
+              </p>
+              <Button label="Reserve a Table" />
+            </section>
+          </article>
+        </div>
         <figure className="cta-image">
           <img src={HomeImage} alt="Serving bruschetta at Little Lemon" />
         </figure>
-      </div>
-    </section>
+      </section>
+      <div className="white-background"></div>
+    </>
   );
 };
 
