07b5b879368d141baf8f29e149d61449a8c89f0c
Michelle <data_Michelle@proton.me>
2025-05-27 05:51:22 -0500

    positioned cta image and content

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import HomeImage from "../assets/serving-bruschetta.png";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="cta-section">
+      <div className="cta-content">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+        <button className="cta-button">Reserve a Table</button>
+      </div>
+      <div className="cta-image">
+        <img src={HomeImage} />
+      </div>
+    </section>
+  );
 };
 
 export default CTA;
--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Specials = () => {
-  return <div>Specials</div>;
+  return <section>Specials</section>;
 };
 
 export default Specials;
